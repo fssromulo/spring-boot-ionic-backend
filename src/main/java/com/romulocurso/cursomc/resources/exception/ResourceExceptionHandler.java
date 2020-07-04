87a9ade649bd73ea3eccdda1e63a35a79c5574bb
romulofss <romulo.fernando@paytrack.com.br>
2020-07-04 17:40:18 -0300

    Ajuste anotacao exception

--- src/main/java/com/romulocurso/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/romulocurso/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -6,10 +6,12 @@ import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objNotFound(ObjectNotFoundException e, HttpServletRequest request) {		
 		StandardError err = new StandardError( HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis() );
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
