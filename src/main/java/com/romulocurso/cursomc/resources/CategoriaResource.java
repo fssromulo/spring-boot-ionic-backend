219d38f548901bb6db7f05115b038fc680d236c2
romulofss <romulo.fernando@paytrack.com.br>
2020-07-14 00:36:19 -0300

    Paginação com paramentos opcionais na requisição

--- src/main/java/com/romulocurso/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/romulocurso/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,7 +29,6 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() { 					
 		List<Categoria> arrCategoria = service.findAll();
@@ -39,10 +40,25 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(arrCategoriaDTO);
 	}
 	
+	@GetMapping(value="/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+		@RequestParam(value="page", defaultValue="0")  Integer page, 
+		@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+		@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+		@RequestParam(value="direction", defaultValue="ASC") String direction				
+	) { 					
+		Page<Categoria> arrCategoria = service.findPage(page, linesPerPage, orderBy, direction);
+		
+		Page<CategoriaDTO> arrCategoriaDTO = arrCategoria.map(
+			categoria -> new CategoriaDTO(categoria)
+		);
+		
+		return ResponseEntity.ok().body(arrCategoriaDTO);
+	}
+	
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<Categoria> find(@PathVariable Integer id) { 
-					
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) { 					
 		Categoria cat1 = service.find(id);
 		return ResponseEntity.ok().body(cat1);
 	}
