c354960c206e080c7d67938a773864efa1a9f7a1
romulofss <romulo.fernando@paytrack.com.br>
2020-08-23 21:50:49 -0300

    Inserindo Pedido

--- src/main/java/com/romulocurso/cursomc/resources/PedidoResource.java
+++ src/main/java/com/romulocurso/cursomc/resources/PedidoResource.java
@@ -1,13 +1,22 @@
 package com.romulocurso.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.romulocurso.cursomc.domain.Categoria;
 import com.romulocurso.cursomc.domain.Pedido;
+import com.romulocurso.cursomc.dto.CategoriaDTO;
 import com.romulocurso.cursomc.services.PedidoService;
 
 @RestController
@@ -23,4 +32,15 @@ public class PedidoResource {
 		Pedido cat1 = service.find(id);
 		return ResponseEntity.ok().body(cat1);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return  ResponseEntity.created(uri).build();			
+	}
+		
 }
--- src/main/java/com/romulocurso/cursomc/services/PedidoService.java
+++ src/main/java/com/romulocurso/cursomc/services/PedidoService.java
@@ -1,11 +1,19 @@
 package com.romulocurso.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.romulocurso.cursomc.domain.ItemPedido;
+import com.romulocurso.cursomc.domain.PagamentoComBoleto;
 import com.romulocurso.cursomc.domain.Pedido;
+import com.romulocurso.cursomc.domain.Produto;
+import com.romulocurso.cursomc.domain.enums.EstadoPagamento;
+import com.romulocurso.cursomc.repositories.ItemPedidoRepository;
+import com.romulocurso.cursomc.repositories.PagamentoRepository;
 import com.romulocurso.cursomc.repositories.PedidoRepository;
 import com.romulocurso.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -14,14 +22,51 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
+
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
 	
 	public Pedido find(Integer idPedido) {
-		Optional<Pedido> obj = repo.findById(idPedido); 		
-		return obj.orElseThrow( () -> 
-			new ObjectNotFoundException(
-					"Objeto não encontrado! Id: " + idPedido + ", Tipo: " + Pedido.class.getName()
-			)
-		);
+		Optional<Pedido> obj = repo.findById(idPedido);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + idPedido + ", Tipo: " + Pedido.class.getName()));
 	}
-	
+
+	@Transactional
+	public Pedido insert(Pedido obj) {
+
+		obj.setId(null);
+		obj.setInstante(new Date());
+
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagtoBoleto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagtoBoleto, obj.getInstante());
+		}
+
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+
+		for (ItemPedido item : obj.getItens()) {
+			item.setDesconto(0.0);		
+			Produto produtoSalvar = produtoService.find(item.getProduto().getId());
+			item.setPreco(produtoSalvar.getPreco());
+			item.setPedido(obj);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+
+	}
 }
